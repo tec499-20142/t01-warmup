974c87adb75b4de52fc30a16dafbc925d5977bcf
Patrick <patrickecomp@gmail.com>
2014-09-22 07:54:43 -0300

    Colocando os testes dentro da pasta tb '-'

--- sim/tb/uartb.vhd
+++ sim/tb/uartb.vhd
@@ -20,6 +20,7 @@ architecture Behavioral of uart_tb is
 	constant MAIN_CLK     : integer := 50;
 	constant BAUD_RATE		:	integer := 9600;	-- Bits per Second
 	constant RST_LVL		:	std_logic := '1';	-- Active Level of Reset
+	constant TEST_RX : std_logic := '0';
 
 	----------------------------------------------
 	-- Signal Declaration
@@ -27,11 +28,12 @@ architecture Behavioral of uart_tb is
 	-- Clock and reset Signals
 	signal clk_50m					:	std_logic := '0';
 	signal rst						:	std_logic;
+	signal test						:	std_logic;
 	-- Transceiver Interface
-	signal data_from_transceiver	:	std_logic;
+	signal data_from_transceiver	:	std_logic := '1';
 	-- uPC Interface
 	signal rx_ready					:	std_logic;
-	signal rx_data					:	std_logic_vector(7 downto 0) := x"5A";
+	signal rx_data					:	std_logic_vector(7 downto 0);
 
 	-- Testbench Signals
 	signal uart_clk					:	std_logic := '0';
@@ -72,9 +74,39 @@ begin
 		wait for (MAIN_CLK_PER*5208)/2;
 		uart_clk	<= not uart_clk;
 	end process;
+	
+	-- RX Clock generation
+	rx_clock_generation:process
+	begin
+		wait for (MAIN_CLK_PER*5100*2);
+		data_from_transceiver	<= not data_from_transceiver; -- start bit
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= not data_from_transceiver; --primeiro bit (1)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= not data_from_transceiver; --segundo bit (0)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= data_from_transceiver; --terceiro bit (0)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= not data_from_transceiver; -- quarto (1)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= data_from_transceiver; -- quinto (1)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= not data_from_transceiver; -- sexto (0)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= not data_from_transceiver; -- setimo (1)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= not data_from_transceiver; -- oitavo (0)
+		wait for (MAIN_CLK_PER*5208);
+		data_from_transceiver	<= not data_from_transceiver; --Stop bit
+		
+	end process;
+	
 
 	-- Reset generation
+
 	rst	<=	RST_LVL, not RST_LVL after MAIN_CLK_PER*5;
-   data_from_transceiver <= '1';
+
+	
+	
 end Behavioral;
 
